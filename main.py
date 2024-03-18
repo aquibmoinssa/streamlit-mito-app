c6e121cd3347addff0b19ddd4e2b582c6e26e8f9
aquibmoinssa <106266370+aquibmoinssa@users.noreply.github.com>
2024-03-18 12:58:41 +0000

    User input

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from mitosheet.streamlit.v1.spreadsheet import _get_mito_backend
 st.set_page_config(layout="wide")
 st.header("AIvigorate")
 st.subheader("Mito AI Implementation on Streamlit")
-uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
+#uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
 def get_data():
+    filepath = st.text_input('Enter your csv file path')
     #csv_url = 'https://drive.google.com/uc?export=download&id=1J_Qa5gpgq0qlT8lg37XDOKp6RJQJZ647'
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_csv(filepath)
     
     #df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv')
     #df = df.drop(0)
