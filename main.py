f6640bfc327a28b3d2cb569a3022c67a03dc0e59
aquibmoinssa <106266370+aquibmoinssa@users.noreply.github.com>
2024-03-18 22:35:36 +0000

    filepath

--- main.py
+++ main.py
@@ -10,16 +10,25 @@ st.header("AIvigorate")
 st.subheader("Mito AI Implementation on Streamlit")
 #uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
-
 def get_data():
     filepath = st.text_input('Enter your csv file path')
+    if filepath:
+        try:
+            df = pd.read_csv(filepath)
+            return df
+        except FileNotFoundError:
+            st.error(f"File '{filepath}' not found.")
+    return None
+
+#def get_data():
+ #   filepath = st.text_input('Enter your csv file path')
     #csv_url = 'https://drive.google.com/uc?export=download&id=1J_Qa5gpgq0qlT8lg37XDOKp6RJQJZ647'
-    df = pd.read_csv(filepath)
+  #  df = pd.read_csv(filepath)
     
     #df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv')
     #df = df.drop(0)
     #df['volume'] = df['volume'].astype(float)
-    return df
+   # return df
 
 test_data = get_data()
 
